15f54110d8c060a1253131133c27ad44b1c18052
aswin krishna <aswinkrishna33716@gmail.com>
2024-04-13 22:19:11 +0530

    first commit

---
+++ api/routes/admin.route.js
@@ -0,0 +1,21 @@
+import express from "express";
+import {
+  blockUser,
+  deleteUser,
+  makeAdmin,
+  userList,
+} from "../controllers/admin.controller.js";
+import { verifyToken, verifyAdmin } from "../utils/verifyUser.js";
+
+const router = express.Router();
+
+router.use(verifyToken);
+
+router.use(verifyAdmin);
+
+router.get("/users", userList);
+router.post("/makeadmin/:id", makeAdmin);
+router.post("/block/:id", blockUser);
+router.delete("/delete/:id", deleteUser);
+
+export default router;
